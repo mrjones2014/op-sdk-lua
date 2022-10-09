15538f81277fa4920578ce2562c7a17a91936391
Mat Jones <mat@mjones.network>
2022-10-09 14:29:17 -0400

    add missing type

--- publish.sh
+++ publish.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
+NEW_VERSION="$1"
+
+if [[ "$NEW_VERSION" = "" ]]; then
+  echo "New version is required as an argument"
+fi
+
+rm "*.rock"
+
+OLD_ROCKSPEC="$(find . -name '*.rockspec')"
+
 luarocks --lua-version=5.1 make --pack-binary-rock
 luarocks --lua-version=5.1 pack op-sdk
+
+luarocks new_version --tag="$NEW_VERSION"
+
+rm "$OLD_ROCKSPEC"
+
+git add .
+git commit -m "prep $NEW_VERSION"
+git tag "$NEW_VERSION"
+git push
+git push --tags
 
 ROCKSPEC="$(find . -name '*.rockspec')"
 
